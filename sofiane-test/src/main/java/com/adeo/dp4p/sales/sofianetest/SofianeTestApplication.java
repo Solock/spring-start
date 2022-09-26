ae1d45233327b7c25ce2b30ec8b4d057dcc04fb6
ZAOUI Sofiane <44767181+Solock@users.noreply.github.com>
2022-09-26 16:43:42 +0200

    Add files via upload

---
+++ sofiane-test/src/main/java/com/adeo/dp4p/sales/sofianetest/SofianeTestApplication.java
@@ -0,0 +1,13 @@
+package com.adeo.dp4p.sales.sofianetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofianeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofianeTestApplication.class, args);
+	}
+
+}
---
+++ sofiane-test/src/test/java/com/adeo/dp4p/sales/sofianetest/SofianeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeo.dp4p.sales.sofianetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofianeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
